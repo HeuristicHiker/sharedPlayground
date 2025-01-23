d6132b49c2cd795e0258a0847ccf6750c0df9425
Connor Johnston <connor@pixelandtexel.com>
2025-01-23 12:30:57 -0500

    Simple 2 sum with tests

--- goLang/basics/main.go
+++ goLang/basics/main.go
@@ -18,24 +18,24 @@ func twoSum(nums []int, target int) []int {
 	return nil
 }
 
+func removeDup(nums []int) int {
+	// assume sorted
+	for idx := 1; idx <= len(nums)-1; idx++ {
+		fmt.Println(idx)
+	}
+	return 0
+}
+
 func main() {
 	tests := []struct {
-		nums     []int
-		target   int
-		expected []int
+		nums   []int
+		target int
 	}{
-		{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
-		{[]int{-3, 4, 3, 90}, 0, []int{0, 2}},
-		{[]int{3, 3}, 6, []int{0, 1}},
-		{[]int{1000000, 2000000, 3000000}, 5000000, []int{1, 2}},
-		{[]int{1, 9, 5, 7}, 8, []int{0, 3}},
-		{[]int{1, 2}, 3, []int{0, 1}},
-		{[]int{-5, -3, -10, -1}, -8, []int{0, 1}},
-		{[]int{-10, -20, -30, -40}, -50, []int{1, 2}},
+		{[]int{1, 1, 2}, 2},
 	}
 
 	currentTest := tests[0]
 
-	fmt.Println(twoSum(currentTest.nums, currentTest.target))
+	fmt.Println(removeDup(currentTest.nums), 2)
 
 }
